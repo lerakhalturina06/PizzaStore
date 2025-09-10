e069708840bd155b74ed09f27e30bbed69db79bb
Valerya Khalturina <lera.khalturina.06@gmail.com>
2025-09-10 14:05:24 +0300

    Initial commit

---
+++ gege2.php
@@ -0,0 +1,9 @@
+<?php
+$v = readline("Введите числа1:");
+$h = readline("Введите числа2:");
+$k = readline("Введите числа3:");
+if ($h - $v === $h - $h) {
+    echo "true";
+} else {
+    echo "false";
+}
