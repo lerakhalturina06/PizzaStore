7e6bc27362dcce39b08386831a3f89a3d31c7fb5
lerakhalturina06 <lera.khalturina.06@gmail.com>
2025-10-29 19:21:30 +0300

    Delete gege2.php

--- gege2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$v = readline("Введите числа1:");
-$h = readline("Введите числа2:");
-$k = readline("Введите числа3:");
-if ($h - $v === $h - $h) {
-    echo "true";
-} else {
-    echo "false";
-}
